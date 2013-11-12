fef125744dfb5843e219a75c2d1498489b9bf926
Frankie Bagnardi <f.bagnardi@gmail.com>
2013-11-12 05:33:53 -0800

    ticket example; fixed math and reorganized code

--- example/ticket/ticket.js
+++ example/ticket/ticket.js
@@ -30,8 +30,11 @@ function TicketViewModel() {
          */
         priceMultiplier: function () {
             if (self.promo.isValid()) {
-                return -2;
+                // this could be hard coded, but I think it's easier to read like this
+                // since it's advertised as "percent off", we need to change it to "percent of"
+                return (100 - 99.3183) / 100;
             }
+            // 1 is the normal price, because it won't modify the price when multiplied
             else {
                 return 1;
             }
@@ -53,12 +56,18 @@ function TicketViewModel() {
 
     /**
      * Buy a ticket
-     * @param which an array containing ticket info
+     * @param {String[]} which an array containing ticket info
      */
     self.buy = function (data) {
+
+        // grab some data from the table
         var transaction = {
             name: data[0],
-            basePrice: data[2]
+
+            // remove all characters except digits and the decimal; then cast them to numbers
+            // so we may do math with them
+            basePrice: Number(data[2].replace(/[^\d.]+/g, '')),
+            fee: Number(data[3].replace(/[^\d.]+/g, ''))
         };
 
         // grab the ticket type
@@ -66,11 +75,12 @@ function TicketViewModel() {
 
         transaction.quantity = 1;
 
-        // increment and decrement function
+        // allow the user to buy more than one ticket
         transaction.increment = function(){
             transaction.quantity++;
         };
 
+        // allow them to buy less, but no less than 1
         transaction.decrement = function(){
             var quantity = transaction.quantity;
             if (quantity > 1) {
@@ -79,24 +89,23 @@ function TicketViewModel() {
         };
 
         // define a custom price property
+        // this checks if they've typed a valid promo, and asks for the multiplier
+        // it then figures out how much `quantity` tickets cost of this type,
+        // and adds the processing fee
         ko.defineProperty(transaction, "price", function () {
-            // remove all characters except digits and decimals
-            var price = transaction.basePrice.replace(/[^\d.]+/g, '');
-
-            console.log(price, transaction.quantity, self.promo.priceMultiplier());
-
             // then get the price by multiplying it by the promo price
-            return self.promo.priceMultiplier() * price * transaction.quantity;
+            return self.promo.priceMultiplier() * transaction.basePrice * transaction.quantity + transaction.fee;
         });
 
-        // only need to track the quantity
+        // only need to track the quantity because nothing else will change
         ko.track(transaction, ["quantity"]);
 
-        // we set it at the end, because otherwise it'll try to update the view before it's implemented
+        // we set it at the end, because otherwise it'll try to update the view before it has all of the properties
+        // which would throw errors
         self.transaction = transaction;
     };
 
-    // default the transaction to nothing
+    // default the transaction to nothing, which can be updated when `buy` is called
     self.transaction = null;
 
     ko.track(self);
