233171b76cd75604f7b8ec17788d89bd713f7d0e
Frankie Bagnardi <f.bagnardi@gmail.com>
2013-11-11 20:27:57 -0800

    ticket example style improvements and cart

--- example/ticket/ticket.js
+++ example/ticket/ticket.js
@@ -1,4 +1,4 @@
-function TicketViewModel(){
+function TicketViewModel() {
     var self = this;
 
     self.ticketInfo = {
@@ -10,16 +10,85 @@ function TicketViewModel(){
         ]
     };
 
+    /**
+     * Handle the promo code and validation of it
+     * You'd realistically do some AJAX in here to confirm that the code is valid
+     * @type {Object}
+     */
     self.promo = ko.track({
         hasPromo: false,
         code: "",
-        isValid: function(){
+        isValid: function () {
             return self.promo.code === "semantic";
         },
-        askForPromo: function(){
+        askForPromo: function () {
             self.promo.hasPromo = true;
+        },
+        /**
+         * this lets us multiply the price depending on the if the promo is valid
+         * @returns {number} the multiplier
+         */
+        priceMultiplier: function () {
+            if (self.promo.isValid()) {
+                return -2;
+            }
+            else {
+                return 1;
+            }
         }
     });
+
+    /**
+     * Allow toggling of some extra information
+     * @type {Object}
+     */
+    self.help = ko.track({
+        showing: false,
+        show: function (data, event) {
+            self.help.showing = !self.help.showing;
+
+            event.preventDefault();
+        }
+    });
+
+    /**
+     * Buy a ticket
+     * @param which an array containing ticket info
+     */
+    self.buy = function (data) {
+        var transaction = {
+            name: data[0],
+            basePrice: data[2]
+        };
+
+        transaction.quantity = 1;
+
+        // increment and decrement function
+        transaction.increment = function(){ transaction.quantity = 1 + Number(transaction.quantity)};
+        transaction.decrement = function(){
+            if (transaction.quantity > 1) {
+                transaction.quantity = 1 - Number(transaction.quantity);
+            }
+        };
+
+        // define a custom price property
+        ko.defineProperty(transaction, "price", function () {
+            // remove all characters except digits and decimals
+            var price = transaction.basePrice.replace(/[^\d.]+/, '');
+
+            // then get the price by multiplying it by the promo price
+            return self.promo.priceMultiplier() * price * transaction.quantity;
+        });
+
+        // only need to track the quantity
+        ko.track(transaction, ["quantity"]);
+
+        // we set it at the end, because otherwise it'll try to update the view before it's implemented
+        self.transaction = transaction;
+    };
+
+    // default the transaction to nothing
+    self.transaction = null;
 
     ko.track(self);
 }
